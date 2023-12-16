ab6aedd3895915a24737e418772e655eaae43dc5
rushaan gandhi <rushaansps@gmail.com>
2023-12-16 23:58:31 +0530

    home-page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
     <div className="app">
       
       <Header />
-      {/*Header*/}
-      {/*Home*/}
+      <Home />
+
     </div>
   );
 }
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        {/* <img className='home__image' src='https://scontent.fdel1-4.fna.fbcdn.net/v/t39.30808-6/240584674_2415206578624258_7938526327995587519_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=ntTAAmOZhXQAX_-6HH3&_nc_ht=scontent.fdel1-4.fna&oh=00_AfBqOwSkWom7LvFuaF1JQWNOvS0z3ZkxzCx55ZbBC2JryA&oe=65836CA2' alt='viszee-500'></img> */}
+      </div>
+    </div>
+  )
+}
+
+export default Home

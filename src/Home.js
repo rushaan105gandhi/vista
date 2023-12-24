7ae1eea85dd02c488a340211eee8bb7b2a1f7afb
rushaan gandhi <rushaansps@gmail.com>
2023-12-24 17:00:18 +0530

    home-page

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import visclave625 from './visclave625.png';
 import './Home.css';
 
 function Home() {
   return (
     <div className='home'>
       <div className='home__container'>
-        {/* <img className='home__image' src='https://scontent.fdel1-4.fna.fbcdn.net/v/t39.30808-6/240584674_2415206578624258_7938526327995587519_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=ntTAAmOZhXQAX_-6HH3&_nc_ht=scontent.fdel1-4.fna&oh=00_AfBqOwSkWom7LvFuaF1JQWNOvS0z3ZkxzCx55ZbBC2JryA&oe=65836CA2' alt='viszee-500'></img> */}
+        <img className='home__image' src={visclave625} alt='visclave'></img>
       </div>
     </div>
   )

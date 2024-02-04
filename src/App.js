e41a9e41de27e82a13ad7f6e76d3e5ade4466108
rushaan gandhi <rushaansps@gmail.com>
2024-02-04 18:04:29 +0530

    changes in package.json and react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      
-      <Header />
-      <Home />
-
-    </div>
+    <Router>
+      <div className="app">
+        <Routes>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
